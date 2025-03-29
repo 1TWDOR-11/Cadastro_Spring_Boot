7c75b104987d615d4df8c3363e561603e7c31da4
HannyaCavalcante <hannyacavalcante@hotmail.com>
2025-03-29 19:22:42 -0300

    Delete iara-games-api directory

--- iara-games-api/src/main/java/com/iaragames/repository/JogoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.iaragames.repository;
-
-import com.iaragames.model.Jogo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JogoRepository extends JpaRepository<Jogo, Long> {}
--- iara-games-api/src/main/java/com/iaragames/repository/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.iaragames.repository;
-
-import com.iaragames.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
